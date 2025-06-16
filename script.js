5934bdea380f0719b42c4a5d3e32f7db6fc65e0f
juanplazas <juan-2151@hotmail.com>
2025-06-15 20:19:43 -0500

    improved methods

--- script.js
+++ script.js
@@ -17,9 +17,7 @@ function uploadCSV() {
 
   reader.onload = async function (e) {
     const content = e.target.result
-    // Aquí puedes hacer algo con el contenido CSV
     console.log('Contenido del CSV:', content)
-    status.textContent = 'Archivo leído correctamente en el navegador.'
 
     const response = await fetch("https://vvuvvvctirpgigmpppowtgr5vu0oxnmm.lambda-url.us-east-1.on.aws/", {
       method: 'POST',
@@ -28,6 +26,15 @@ function uploadCSV() {
       },
       body: content
     });
+
+    const responseText = await response.text();
+
+    if (!response.ok) {
+      const errorData = JSON.parse(responseText);
+      throw new Error(`Error HTTP: ${response.status} - ${response.statusText}. Detalles: ${JSON.stringify(errorData, null, 2)}`);
+    }
+
+    status.textContent = responseText
 
     console.log(response)
   }
@@ -42,19 +49,13 @@ function uploadCSV() {
 async function initialLoad() {
   let fechaFinal = new Date()
   let fechaInicial = new Date(fechaFinal)
+  fechaInicial.setMonth(fechaInicial.getMonth() - 1)
 
-  console.log("fechaFinal1", fechaFinal)
-  console.log("fechaInicial1", fechaInicial)
-
-  fechaInicial = `${fechaInicial.getFullYear()}-${fechaInicial.getMonth() < 10 ? '0'+fechaInicial.getMonth() : fechaInicial.getMonth()  }-${fechaInicial.getDate() < 10 ? '0'+ fechaInicial.getDate() : fechaInicial.getDate()}` 
-  fechaFinal = `${fechaFinal.getFullYear()}-${fechaFinal.getMonth() < 10 ? '0'+fechaFinal.getMonth() : fechaFinal.getMonth()}-${fechaFinal.getDate() < 10 ? '0'+ fechaFinal.getDate() : fechaFinal.getDate()}` 
+  fechaInicial = `${fechaInicial.getFullYear()}-${fechaInicial.getMonth() < 10 ? '0' + (fechaInicial.getMonth() + 1) : fechaInicial.getMonth() + 1}-${fechaInicial.getDate() < 10 ? '0' + fechaInicial.getDate() : fechaInicial.getDate()}`
+  fechaFinal = `${fechaFinal.getFullYear()}-${fechaFinal.getMonth() < 10 ? '0' + (fechaFinal.getMonth() + 1) : fechaFinal.getMonth() + 1}-${fechaFinal.getDate() < 10 ? '0' + fechaFinal.getDate() : fechaFinal.getDate()}`
 
   const datepickerInicial = document.getElementById('datepickerInicial');
   const datepickerFinal = document.getElementById('datepickerFinal');
-
-  console.log("fechaFinal2", fechaFinal)
-  console.log("fechaInicial2", fechaInicial)
-
 
   datepickerInicial.value = fechaInicial
   datepickerFinal.value = fechaFinal
@@ -71,17 +72,35 @@ async function initialLoad() {
   const responseText = await response.text();
 
   if (!response.ok) {
-    const errorData = JSON.parse(responseText); // Intentar parsear el error si es JSON
+    const errorData = JSON.parse(responseText);
     throw new Error(`Error HTTP: ${response.status} - ${response.statusText}. Detalles: ${JSON.stringify(errorData, null, 2)}`);
   }
 
-  const data = JSON.parse(responseText); // Parsear la cadena JSON a un objeto JavaScript
-  console.log("data", data);
+  const data = JSON.parse(responseText);
   renderTabla(data)
 }
 
+async function getDataByDates() {
+  const fechaInicial = document.getElementById('datepickerInicial').value;
+  const fechaFinal = document.getElementById('datepickerFinal').value;
+  const query = encodeURIComponent(`select * from ventas.vetas_detalle vd where fecha BETWEEN '${fechaInicial} 00:00:00' and '${fechaFinal} 00:00:00'`);
+  const response = await fetch(`https://vvuvvvctirpgigmpppowtgr5vu0oxnmm.lambda-url.us-east-1.on.aws/?query=${query}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
+  const responseText = await response.text();
+  if (!response.ok) {
+    const errorData = JSON.parse(responseText);
+    throw new Error(`Error HTTP: ${response.status} - ${response.statusText}. Detalles: ${JSON.stringify(errorData, null, 2)}`);
+  }
+  const data = JSON.parse(responseText);
+  renderTabla(data)
 
-async function renderTabla(data) { 
+}
+
+async function renderTabla(data) {
   const tableBody = document.getElementById('bodyTable');
   tableBody.innerHTML = '';
 
@@ -90,8 +109,8 @@ async function renderTabla(data) {
     rowData = data[i]
     for (let j = 0; j < rowData.length; j++) {
       const td = document.createElement('td');
-      td.textContent = rowData[j]; // Asignar el valor del elemento
-      row.appendChild(td); // Añadir <td> a la fila
+      td.textContent = rowData[j];
+      row.appendChild(td);
     }
 
     tableBody.appendChild(row);
